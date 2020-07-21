76a1aec363c3fe4defb4837c95b8ed602b991e03
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-21 20:58:14 +0530

    Added functionality to select brush size from custom dialog

--- app/src/main/java/com/subhipandey/android/drawingapp/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/drawingapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.subhipandey.android.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         drawing_view.setSizeForBrush(20.toFloat())
+
+        ib_brush.setOnClickListener {
+            showBrushChooserDialog()
+        }
+    }
+
+    private fun showBrushChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+        val smallBtn = brushDialog.ib_small_brush
+        smallBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn = brushDialog.ib_medium_brush
+        mediumBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.ib_large_brush
+        largeBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
