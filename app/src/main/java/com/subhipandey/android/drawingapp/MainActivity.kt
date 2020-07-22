f10df20fb13b265f00880f44a02348978f164237
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-22 12:29:23 +0530

    Added Color Pallete

--- app/src/main/java/com/subhipandey/android/drawingapp/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/drawingapp/MainActivity.kt
@@ -3,22 +3,37 @@ package com.subhipandey.android.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawing_view.setSizeForBrush(20.toFloat())
 
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pallet_pressed
+            )
+        )
+
+
         ib_brush.setOnClickListener {
-            showBrushChooserDialog()
+            showBrushSizeChooserDialog()
         }
     }
 
-    private fun showBrushChooserDialog() {
+    private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush Size: ")
