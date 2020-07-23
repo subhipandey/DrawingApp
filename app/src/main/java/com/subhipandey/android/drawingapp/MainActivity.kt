bfa350f49fe92a6855edd44c65e1b02d8121e411
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-23 19:39:40 +0530

    Added functionality to save image

--- app/src/main/java/com/subhipandey/android/drawingapp/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/drawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -20,6 +21,9 @@ import androidx.core.provider.FontsContractCompat.FontRequestCallback.RESULT_OK
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.lang.Exception
 import java.util.jar.Manifest
 
@@ -63,6 +67,14 @@ class MainActivity : AppCompatActivity() {
 
         ib_undo.setOnClickListener {
             drawing_view.onClickUndo()
+        }
+
+        ib_save.setOnClickListener {
+            if(isReadStorageAllowed()){
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
+            }else{
+                requestStoragePermission()
+            }
         }
     }
 
@@ -182,14 +194,58 @@ class MainActivity : AppCompatActivity() {
         val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(returnedBitmap)
         val bgDrawable = view.background
-        if(bgDrawable != null){
+        if (bgDrawable != null) {
             bgDrawable.draw(canvas)
 
-        }else{
+        } else {
             canvas.drawColor(Color.WHITE)
         }
         view.draw(canvas)
         return returnedBitmap
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap) : AsyncTask<Any, Void, String>() {
+        override fun doInBackground(vararg p0: Any?): String {
+            var result = ""
+            if (mBitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val f =
+                        File(
+                            externalCacheDir!!.absoluteFile.toString()
+                                    + File.separator + "ChildrenDrawingApp_"
+                                    + System.currentTimeMillis() / 1000 + ".png"
+                        )
+
+                    val fos = FileOutputStream(f)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result = f.absolutePath
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!result!!.isEmpty()) {
+                Toast.makeText(
+                    this@MainActivity, "File Saved Successfully :$result",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong while saving the file.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     }
 
     companion object {
